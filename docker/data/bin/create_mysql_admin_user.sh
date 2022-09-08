7a708a9b971d42cfe4f229b6eb3729b72c8805f2
lachlan <lachlan.00@gmail.com>
2022-09-08 13:57:04 +1000

    use the php8.1 docker

--- docker/data/bin/create_mysql_admin_user.sh
+++ docker/data/bin/create_mysql_admin_user.sh
@@ -4,7 +4,7 @@ mysqld_safe &
 sleep 5
 
 RET=1
-while [[ $RET -ne 0 ]]; do
+while [ $RET -ne 0 ]; do
     echo "=> Waiting for confirmation of MySQL service startup"
     sleep 5
     mysql -uroot -e "status" > /dev/null 2>&1
